9737fad055c5274759e12c007324f80f4dbb578f
Max Miller <maxmiller413@gmail.com>
2021-01-31 01:16:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,6 @@ end
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? do |string|
     string.length > 4
-    
   end 
 end
 
@@ -27,6 +26,9 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
  
+  planeteer_calls.first do |calls|
+   calls == valid_calls
+  end 
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
 end
